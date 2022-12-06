685460a8f4439fc12b3f0827753403c4750bc623
Dangerforce <joeziet@gmail.com>
2022-12-05 22:00:54 -0500

    Day 2

--- Day3.py
+++ Day3.py
@@ -1,6 +1,4 @@
-
 with open('input3.txt', 'r') as f:
-        
         lst = f.readlines()
         print("Part One")
         print(sum([x -96 if x >= 97 else x-38 for x in [ ord(list(set(s[:int(len(s)/2)]).intersection(s[int(len(s)/2):]))[0]) for s in lst]]))
